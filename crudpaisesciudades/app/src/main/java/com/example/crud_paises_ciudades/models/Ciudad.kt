8bb8130a002d95a74a80d81371cf4a4c68003b32
domepl <108755862+domepl@users.noreply.github.com>
2024-12-09 01:24:43 -0500

    Delete crudpaisesciudades/app/src/main/java/com/example/crud_paises_ciudades/models directory

--- crudpaisesciudades/app/src/main/java/com/example/crud_paises_ciudades/models/Ciudad.kt
+++
@@ -1,9 +0,0 @@
-package com.example.crud_paises_ciudades.models
-
-data class Ciudad(
-    val nombre: String,
-    val poblacion: Int,
-    val area: Double,
-    val tieneAeropuerto: Boolean,
-    val fechaFundacion: String
-)
